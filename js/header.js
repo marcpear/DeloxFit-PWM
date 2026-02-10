3d9339aa25b0baa6be17d7a76eb4d8de0c8b59b1
marcos <marcospear@gmail.com>
2026-02-10 20:09:36 +0000

    Primera versión header

---
+++ js/header.js
@@ -0,0 +1,20 @@
+const headerTemplate = `
+    <header class="main-header">
+        <div class="logo-box">
+            <img src="assets/logo.png" alt="Logo DeloxFit" class="logoDF">
+        </div>
+
+        <nav class="nav-menu">
+            <ul>
+                <li><a href="#">Conócenos</a></li>
+                <li><a href="#">Instalaciones</a></li>
+                <li><a href="#">Clases</a></li>
+                <li><a href="#">Suscripciones</a></li>
+                <li><a href="#">Inscripción</a></li>
+            </ul>
+        </nav>
+    </header>
+`;
+
+// Inyectamos el código en el HTML
+document.getElementById('header-template').innerHTML = headerTemplate;
