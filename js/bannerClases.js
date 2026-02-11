7db661be9c35675dcf505fa9bb1e960dfbf27a9c
marcos <marcospear@gmail.com>
2026-02-11 17:12:32 +0000

    Cambios en el header y Banner V1

---
+++ js/bannerClases.js
@@ -0,0 +1,14 @@
+const  bannerTemplate = `
+<div class="background">
+            <img src="assets/imgym1.jpg" alt="back bannerC" class="background">
+        </div>
+        
+<nav class="botonB">
+            <ul>
+                <a href="#" class="botonB">CLASES</a>
+            </ul>
+        </nav>
+        
+`;
+
+  document.getElementById('banner-template').innerHTML = bannerTemplate;
